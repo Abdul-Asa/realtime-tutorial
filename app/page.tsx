feb20c7fa9426ce195baabc205db77aee2fb6a6b
Your Name <bigtulahi1@gmaiil.com>
2023-11-01 22:01:25 +0000

    first step

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { useEffect, useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
 import supabaseClient from "../wrappers/supabase-client";
 import { generateRandomName } from "@/lib/utils";
 import ThemeSwitch from "@/components/theme-switch";
 import { nanoid } from "nanoid";
-import { RealtimeClient } from "@supabase/supabase-js";
-
+// import _ from "lodash";
+import { useMousePosition } from "@/lib/hooks/use-mouse";
+import { on } from "events";
+import { set } from "lodash";
 type User = {
   user_id: string;
   username: string;
@@ -13,60 +15,60 @@ type User = {
 };
 
 const userId = nanoid();
-const userName = generateRandomName();
+const localName = generateRandomName();
 export default function Home() {
   const [users, setUsers] = useState<any[]>([]);
-  const [newUsername, setNewUsername] = useState<string>(userName);
-
-  const handleJoin = ({ newPresences }: any) => {
-    setUsers((prevUsers) => {
-      const updatedUsers = [...prevUsers];
-      newPresences.forEach((presence: any) => {
-        const newUser: User = {
-          user_id: presence.user_id,
-          username: presence.username,
-          join_time: presence.join_time,
-        };
-        updatedUsers.push(newUser);
-      });
-      return updatedUsers;
-    });
-  };
-
-  const handleLeave = ({ leftPresences }: any) => {
-    setUsers((prevUsers) => {
-      const updatedUsers = prevUsers.filter(
-        (user) =>
-          !leftPresences.some(
-            (leftPresence: any) => leftPresence.user_id === user.user_id
-          )
-      );
-      return updatedUsers;
-    });
-  };
+  const [myJoinTime, setMyJoinTime] = useState<any>(
+    new Date().toLocaleTimeString()
+  );
+  const [newUsername, setNewUsername] = useState<string>(localName);
+  // const [initialSync, setInitialSync] = useState<boolean>(false);
+  const { x, y } = useMousePosition();
+  const sendNameChange = useRef<(updName: string) => void>();
 
   useEffect(() => {
     const onlineChannel = supabaseClient.channel("online");
-
     onlineChannel
-      .on("presence", { event: "join" }, handleJoin)
-      .on("presence", { event: "leave" }, handleLeave)
+      .on("presence", { event: "sync" }, () => {
+        const state = onlineChannel.presenceState();
+        const presences = Object.values(state);
+        setUsers(presences.flat());
+      })
       .subscribe(async (status) => {
         if (status !== "SUBSCRIBED") {
           return;
         }
         const userStatus = {
           user_id: userId,
-          username: userName,
+          username: localName,
           join_time: new Date().toLocaleTimeString(),
         };
         await onlineChannel.track(userStatus);
       });
 
+    sendNameChange.current = async (updName) => {
+      console.log("sending name change");
+      console.log(myJoinTime, updName);
+      await onlineChannel.track({
+        username: updName,
+        join_time: myJoinTime,
+        user_id: userId,
+      });
+    };
+
+    // setInitialSync(true);
     return () => {
-      onlineChannel && supabaseClient.removeChannel(onlineChannel);
+      onlineChannel.unsubscribe();
+      supabaseClient.removeChannel(onlineChannel);
     };
   }, []);
+
+  useEffect(() => {
+    if (users.length === 0) return;
+    const myUser = users.find((user) => user.user_id === userId);
+    if (!myUser) return;
+    setMyJoinTime(myUser.join_time);
+  }, [users]);
 
   return (
     <main className="relative flex w-screen h-screen max-h-screen p-4 overflow-hidden">
@@ -83,18 +85,33 @@ export default function Home() {
       </div>
       <div className="flex flex-col justify-between w-full h-full">
         <div className="flex justify-between">
-          <div className="w-20 h-20 bg-blue-400"></div>
+          <div className="w-20 h-20 bg-blue-400">
+            <input
+              type="text"
+              value={newUsername}
+              onChange={(e) => setNewUsername(e.target.value)}
+              placeholder="Enter new username"
+            />
+            <button onClick={() => sendNameChange.current?.(newUsername)}>
+              Change Username
+            </button>
+          </div>
           <div className="">
             List of online users:
-            {users.map((user, ind) => (
-              <div
+            {users.map((user) => (
+              <p
                 key={user.user_id}
                 className={user.user_id === userId ? "text-green-200" : ""}
               >
                 {user.username} joined at: {user.join_time}
-              </div>
+              </p>
             ))}
           </div>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p>
+            my position: {x} and {y}
+          </p>
         </div>
         <div className="flex items-end justify-between">
           <div className="flex items-center space-x-4">
